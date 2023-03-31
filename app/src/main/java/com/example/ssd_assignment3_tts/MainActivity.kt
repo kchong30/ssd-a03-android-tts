727488e958163aaf8c33bdea94e846f91440c0bb
Kevin Chong <kchong30@my.bcit.ca>
2023-03-31 05:37:54 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/ssd_assignment3_tts/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.ssd_assignment3_tts
+
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var inputText: EditText
+    private lateinit var speakButton: Button
+    private lateinit var textToSpeech: TextToSpeech
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        inputText = findViewById(R.id.input_text)
+        speakButton = findViewById(R.id.speak_button)
+        textToSpeech = TextToSpeech(this, this)
+
+        speakButton.setOnClickListener {
+            val text = inputText.text.toString()
+            speakOut(text)
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.CANADA)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                println("Language not supported")
+            }
+        } else {
+            println("Initialization failed")
+        }
+    }
+
+    private fun speakOut(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
+}
